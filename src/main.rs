19ebf6fd3f5eeac6c89ecd246de99c3d21deab9b
JJ Chen <jiajiechen95@gmail.com>
2024-01-21 20:35:38 -0500

    Setup initial game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+/* For porting `rltk` to `bracket_lib`:
+ *   `rltk::` -> `bracket_lib::prelude::`
+ *   `Rltk` -> `BTerm` (and so on for builders)
+ */
+use bracket_lib::prelude::{BTerm, GameState};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World");
+    }
+}
+
+fn main() -> bracket_lib::prelude::BError {
+    use bracket_lib::prelude::BTermBuilder;
+    let context = BTermBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+    let gs = State {};
+    bracket_lib::prelude::main_loop(context, gs)
 }
